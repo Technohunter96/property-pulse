9c28abb9a4857e257eef725bdeb99bb177c62206
Michal <michalhavlik2@gmail.com>
2024-03-27 14:58:43 +0100

    delete on cloudinary

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -1,6 +1,7 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import { getSessionUser } from "@/utils/getSessionUser";
+import cloudinary from "@/config/cloudinary";
 
 // GET /api/properties/:id
 export const GET = async (request, { params }) => {
@@ -44,6 +45,19 @@ export const DELETE = async (request, { params }) => {
     // Verify ownership
     if (property.owner.toString() !== userId) {
       return new Response("Unauthorized", { status: 401 });
+    }
+
+    // Extract public id's from image url in DB
+    const publicIds = property.images.map((imageUrl) => {
+      const parts = imageUrl.split("/");
+      return parts.at(-1).split(".").at(0);
+    });
+
+    // Delete images from cloudinary
+    if (publicIds.length > 0) {
+      for (let publicId of publicIds) {
+        await cloudinary.uploader.destroy("propertypulse/" + publicId);
+      }
     }
 
     await property.deleteOne();
