9c1ef488f491620387229cc0df85c6dc3193cdfa
Michal <michalhavlik2@gmail.com>
2024-03-27 13:02:37 +0100

    Featured Properties

--- utils/requests.js
+++ utils/requests.js
@@ -1,14 +1,17 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
 // Fetching all properties /api/properties
-async function fetchProperties() {
+async function fetchProperties({ showFeatured = false } = {}) {
   try {
     // Handle the case where the domain is not available yet
     if (!apiDomain) {
       return [];
     }
 
-    const res = await fetch(`${apiDomain}/properties`, { cache: "no-store" });
+    const res = await fetch(
+      `${apiDomain}/properties${showFeatured ? "/featured" : ""}`, // Showing featured properties if there are any
+      { cache: "no-store" }
+    );
 
     if (!res.ok) {
       throw new Error("Failed to fetch data");
